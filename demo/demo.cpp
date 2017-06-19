8c9f028f40c29523794d94b857e11efea685b0f1
Benjamin Van Treese <vantreeseba@gmail.com>
2017-06-19 00:03:34 -0500

    initial commit

---
+++ demo/demo.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+
+int main (void)
+{
+  hello_world();
+  return 0;
+}
---
+++ lib/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void hello_world()
+{
+  std::cout << "hello world!" << std::endl;
+}
